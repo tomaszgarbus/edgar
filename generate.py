ca0843cd20909959ea2ae5d7da15e4d68197fb26
Tomasz Garbus <tgarbus@chromium.org>
2020-05-20 23:20:17 +0200

    Add generate function

---
+++ generate.py
@@ -0,0 +1,38 @@
+from typing import Set, Tuple
+
+import gpt_2_simple.gpt_2 as gpt2
+from ner.highlighter import Highlighter
+from utils import f1
+
+
+def generate(prompt: str, hl: Highlighter,
+             entity_set: Set[Tuple[str, str]], model_name='355M',
+             multi_gpu=False,
+             model_dir='gpt_2_simple/models',
+             checkpoint_dir='gpt_2_simple/checkpoints',
+             steps: int = 5, step_length: int = 20,
+             samples_per_step: int = 20):
+    sess = gpt2.start_tf_sess()
+
+    gpt2.load_gpt2(sess, checkpoint_dir=checkpoint_dir,
+                   model_name=model_name, multi_gpu=multi_gpu,
+                   model_dir=model_dir)
+
+    text = prompt
+    for _ in range(steps):
+        variants = gpt2.generate(sess,
+                                 checkpoint_dir=checkpoint_dir,
+                                 model_name=model_name,
+                                 model_dir=model_dir,
+                                 return_as_list=True,
+                                 nsamples=samples_per_step,
+                                 length=step_length,
+                                 prefix=text)
+        variants = list(map(
+            lambda v: (f1(entity_set, hl.extract_entities_from_article(v)), v),
+            variants))
+        variants = list(map(lambda v: (0. if v[0] is None else v[0], v[1]),
+                            variants))
+        variants.sort(reverse=True)
+        text = text + variants[0][1]
+    return text
--- utils.py
+++ utils.py
@@ -1,10 +1,37 @@
 import json
 import logging
 import os
+from typing import Optional
 
 from jsonschema import validate
 
 from ner.schema import schema as config_schema
+
+
+def precision(y, p) -> Optional[float]:
+    if len(p) != 0:
+        return len(p.intersection(y)) / len(p)
+    else:
+        return None
+
+
+def recall(y, p) -> Optional[float]:
+    if len(y) != 0:
+        return len(p.intersection(y)) / len(y)
+    else:
+        return None
+
+
+def f1(y, p) -> Optional[float]:
+    prec = precision(y, p)
+    rec = recall(y, p)
+    if prec is not None and rec is not None:
+        if prec + rec == 0.:
+            return 0.
+        else:
+            return 2 * prec * rec / (prec + rec)
+    else:
+        return None
 
 
 def list_all_files(dir):
