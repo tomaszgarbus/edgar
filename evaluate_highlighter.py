cc64534c820793c34534ff754818c10f74b6d4d0
Tomasz Garbus <tgarbus@chromium.org>
2020-05-20 21:01:20 +0200

    Make evaluate_category function public

--- evaluate_highlighter.py
+++ evaluate_highlighter.py
@@ -1,35 +1,45 @@
 from typing import List, Tuple, Optional, Set
 
 import numpy as np
+from tqdm import tqdm
 
 from ner.highlighter import Highlighter
 
 
-def _evaluate_category(hl: Highlighter, texts: List[str],
-                       entity_sets: List[Set[Tuple[str, str]]],
-                       category_name: Optional[str]) -> None:
+def evaluate_category(hl: Highlighter, texts: List[str],
+                      entity_sets: List[Set[Tuple[str, str]]],
+                      category_name: Optional[str]) -> None:
+    """
+    Evaluates the Highlighter only on one category of entities.
+    :param hl: Highlighter object put under test.
+    :param texts: Evaluation dataset.
+    :param entity_sets: Ground truths for each text, expressed as sets
+                        of pairs (base entity name, category name).
+    :param category_name: If None, all categories are evaluated jointly.
+    :return: Nothing, outputs evaluation metrics to std.
+    """
     precs = []
     recs = []
     f1s = []
-    for ground_truth, text in zip(entity_sets, texts):
+    for ground_truth, text in tqdm(zip(entity_sets, texts)):
         pred = hl.extract_entities_from_article(text)
         y = ground_truth
         p = pred
         if category_name is not None:
             y = set(filter(lambda a: a[1] == category_name, y))
             p = set(filter(lambda a: a[1] == category_name, p))
-            if len(y) != 0:
-                rec = len(p.intersection(y)) / len(y)
-                recs.append(rec)
-            if len(p) != 0:
-                prec = len(p.intersection(y)) / len(p)
-                precs.append(prec)
-            if len(y) != 0 and len(p) != 0:
-                if prec + rec == 0.:
-                    f1 = 0.
-                else:
-                    f1 = 2 * prec * rec / (prec + rec)
-                f1s.append(f1)
+        if len(y) != 0:
+            rec = len(p.intersection(y)) / len(y)
+            recs.append(rec)
+        if len(p) != 0:
+            prec = len(p.intersection(y)) / len(p)
+            precs.append(prec)
+        if len(y) != 0 and len(p) != 0:
+            if prec + rec == 0.:
+                f1 = 0.
+            else:
+                f1 = 2 * prec * rec / (prec + rec)
+            f1s.append(f1)
     if category_name is not None:
         print('Category \"%s\":' % category_name)
     else:
@@ -54,4 +64,4 @@ def evaluate(hl: Highlighter, texts: List[str],
         for e in es:
             categories.add(e[1])
     for category in list(categories) + [None]:
-        _evaluate_category(hl, texts, entity_sets, category)
+        evaluate_category(hl, texts, entity_sets, category)
